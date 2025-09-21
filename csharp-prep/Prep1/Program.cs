e436abe3ea91f561d11f2be3233da2926d54824b
highly esteemed Louis Sanchez <san23101@byui.edu>
2025-09-20 20:47:14 -0600

    Finished prep 1

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,6 +4,13 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
+        Console.Write("What is your first name? ");
+        String first = Console.ReadLine();
+
+        Console.Write("what is your Last namwe? ");
+        String last = Console.ReadLine();
+
+        Console.WriteLine($" Your name is {last}, {first} {last}");
+
     }
 }
